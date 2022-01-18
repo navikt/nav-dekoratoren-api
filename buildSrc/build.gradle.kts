1dc04a1e50c140c0a50e19cd080fe7ac4dfd307d
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2022-01-18 08:46:27 +0100

    chore: bump dittnav dependencies version to 2022.01.13-08.31-581d3f963a95

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.01.28-13.10-57381c88aaab"
+val dittNavDependenciesVersion = "2022.01.13-08.31-581d3f963a95"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
