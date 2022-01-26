1bbcbe654b18d00d8c68dbc0f038c34d93962c91
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2022-01-26 08:26:25 +0100

    refactor: replace token-support config mapping with library provided function

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/oidc/OidcTokenValidator.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/oidc/OidcTokenValidator.kt
@@ -7,7 +7,7 @@ import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration
 import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever
 import no.nav.security.token.support.core.jwt.JwtToken
 import no.nav.security.token.support.core.validation.JwtTokenValidationHandler
-import java.net.URL
+import no.nav.security.token.support.ktor.asIssuerProps
 
 class OidcTokenValidator constructor(applicationConfig: ApplicationConfig) {
 
@@ -18,20 +18,7 @@ class OidcTokenValidator constructor(applicationConfig: ApplicationConfig) {
     private val multiIssuerConfiguration: MultiIssuerConfiguration
 
     init {
-        val issuerPropertiesMap: Map<String, IssuerProperties> =
-            applicationConfig.configList("no.nav.security.jwt.issuers")
-                .associate { issuerConfig ->
-                    val discoveryurl = URL(issuerConfig.property("discoveryurl").getString())
-                    val acceptedAudience = issuerConfig.property("accepted_audience").getString().split(",")
-                    val cookieName = issuerConfig.propertyOrNull("cookie_name")?.getString()
-                    val properties = IssuerProperties(discoveryurl, acceptedAudience, cookieName)
-
-                    issuerConfig.propertyOrNull("optional_claims")?.getString()?.split(",")?.let { optionalClaims ->
-                        properties.validation = IssuerProperties.Validation(optionalClaims)
-                    }
-
-                    issuerConfig.property("issuer_name").getString() to properties
-                }
+        val issuerPropertiesMap: Map<String, IssuerProperties> = applicationConfig.asIssuerProps()
 
         multiIssuerConfiguration = MultiIssuerConfiguration(issuerPropertiesMap, resourceRetriever)
 
