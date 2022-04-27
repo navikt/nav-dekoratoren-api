9c86595ea1c240709ce6a8a1f915d7dd3ed8101c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-04-27 14:01:26 +0200

    Lagt til maximumSize og expireAfterWrite på pdl-cache

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/user/SubjectNameService.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/user/SubjectNameService.kt
@@ -2,12 +2,16 @@ package no.nav.personbruker.innloggingsstatus.user
 
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
+import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.runBlocking
 import no.nav.personbruker.innloggingsstatus.pdl.PdlService
 
 class SubjectNameService(private val pdlService: PdlService) {
 
-    private val cache: Cache<String, String> = Caffeine.newBuilder().build()
+    private val cache: Cache<String, String> = Caffeine.newBuilder()
+        .maximumSize(1024)
+        .expireAfterWrite(15, TimeUnit.MINUTES)
+        .build()
 
     suspend fun getSubjectName(subject: String): String {
         return cache.get(subject) {
