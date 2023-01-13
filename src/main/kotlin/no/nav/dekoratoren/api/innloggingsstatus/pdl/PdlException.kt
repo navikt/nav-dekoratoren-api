6f19241c2de0d34fe85e584a7b69cb931b76d590
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-01-13 14:55:00 +0100

    Forbedret feilhåndtering i PDL-consumer. Fjernet skjør håndtering av feil og logger heller hele responsen fra PDL som det oppfordres til i docs.

--- src/main/kotlin/no/nav/dekoratoren/api/innloggingsstatus/pdl/PdlException.kt
+++ src/main/kotlin/no/nav/dekoratoren/api/innloggingsstatus/pdl/PdlException.kt
@@ -3,5 +3,4 @@ package no.nav.dekoratoren.api.innloggingsstatus.pdl
 open class PdlException : Exception {
     constructor() : super()
     constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
 }
