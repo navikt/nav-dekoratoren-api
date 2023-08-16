076371b8e5cd74b49e3d96febdfe08f88e5ae3cd
Trong Huu Nguyen <tronghn@users.noreply.github.com>
2023-08-16 14:21:18 +0200

    remove loginservice cookie validation (#69)

--- src/test/kotlin/no/nav/dekoratoren/api/innloggingsstatus/auth/AuthTokenServiceTest.kt
+++ src/test/kotlin/no/nav/dekoratoren/api/innloggingsstatus/auth/AuthTokenServiceTest.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.runBlocking
 import no.nav.dekoratoren.api.innloggingsstatus.oidc.OidcTokenInfo
 import no.nav.dekoratoren.api.innloggingsstatus.oidc.OidcTokenService
 import no.nav.dekoratoren.api.innloggingsstatus.user.SubjectNameService
-import no.nav.dekoratoren.api.innloggingsstatus.wonderwall.WonderwallTokenService
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
@@ -22,12 +21,10 @@ internal class AuthTokenServiceTest {
 
     private val oidcTokenService: OidcTokenService = mockk()
     private val subjectNameService: SubjectNameService = mockk()
-    private val wonderwallTokenService: WonderwallTokenService = mockk()
 
-    private val authTokenService = no.nav.dekoratoren.api.innloggingsstatus.auth.AuthTokenService(
+    private val authTokenService = AuthTokenService(
         oidcTokenService,
         subjectNameService,
-        wonderwallTokenService,
     )
 
     private val call: ApplicationCall = mockk()
@@ -42,7 +39,6 @@ internal class AuthTokenServiceTest {
         )
 
         coEvery { oidcTokenService.getOidcToken(call) } returns tokenInfo
-        coEvery { wonderwallTokenService.getToken(call) } returns null
         coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
 
         val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
@@ -53,29 +49,8 @@ internal class AuthTokenServiceTest {
     }
 
     @Test
-    fun `should provide correct info for self issued token`() {
-        val tokenInfo = OidcTokenInfo(
-            subject = subject1,
-            authLevel = 4,
-            issueTime = LocalDateTime.now(),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-
-        coEvery { oidcTokenService.getOidcToken(call) } returns null
-        coEvery { wonderwallTokenService.getToken(call) } returns tokenInfo
-        coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
-
-        val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
-
-        subjectInfo.authenticated `should be equal to` true
-        subjectInfo.name `should be equal to` subject1Name
-        subjectInfo.securityLevel `should be equal to` "4"
-    }
-
-    @Test
     fun `should provide correct info when unauthenticated`() {
         coEvery { oidcTokenService.getOidcToken(call) } returns null
-        coEvery { wonderwallTokenService.getToken(call) } returns null
         coEvery { subjectNameService.getSubjectName(any()) } returns subject1Name
 
         val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
@@ -95,7 +70,6 @@ internal class AuthTokenServiceTest {
         )
 
         coEvery { oidcTokenService.getOidcToken(call) } returns oidcTokenInfo
-        coEvery { wonderwallTokenService.getToken(call) } returns null
         coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
 
         val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
@@ -103,79 +77,5 @@ internal class AuthTokenServiceTest {
         subjectInfo.authenticated `should be equal to` true
         subjectInfo.name `should be equal to` subject1Name
         subjectInfo.securityLevel `should be equal to` "4"
-    }
-
-    @Test
-    fun `should defer to oidc token if it is newer than wonderwall token`() {
-        val oidcTokenInfo = OidcTokenInfo(
-            subject = subject1,
-            authLevel = 3,
-            issueTime = LocalDateTime.now(),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-        val wonderwallTokenInfo = OidcTokenInfo(
-            subject = subject2,
-            authLevel = 4,
-            issueTime = LocalDateTime.now().minusMinutes(5),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-
-        coEvery { oidcTokenService.getOidcToken(call) } returns oidcTokenInfo
-        coEvery { wonderwallTokenService.getToken(call) } returns wonderwallTokenInfo
-        coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
-        coEvery { subjectNameService.getSubjectName(subject2) } returns subject2Name
-
-        val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
-
-        subjectInfo.authenticated `should be equal to` true
-        subjectInfo.name `should be equal to` subject1Name
-        subjectInfo.securityLevel `should be equal to` "3"
-    }
-
-    @Test
-    fun `should defer to self-issued token if it is newer than oidc token`() {
-        val oidcTokenInfo = OidcTokenInfo(
-            subject = subject1,
-            authLevel = 3,
-            issueTime = LocalDateTime.now().minusMinutes(5),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-        val wonderwallTokenInfo = OidcTokenInfo(
-            subject = subject2,
-            authLevel = 4,
-            issueTime = LocalDateTime.now(),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-
-        coEvery { oidcTokenService.getOidcToken(call) } returns oidcTokenInfo
-        coEvery { wonderwallTokenService.getToken(call) } returns wonderwallTokenInfo
-        coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
-        coEvery { subjectNameService.getSubjectName(subject2) } returns subject2Name
-
-        val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
-
-        subjectInfo.authenticated `should be equal to` true
-        subjectInfo.name `should be equal to` subject2Name
-        subjectInfo.securityLevel `should be equal to` "4"
-    }
-
-    @Test
-    fun `should claim user is unauthenticated if self-issued token service throws error and oidc service is ok`() {
-        val subject1OidcTokenInfo = OidcTokenInfo(
-            subject = subject1,
-            authLevel = 3,
-            issueTime = LocalDateTime.now(),
-            expiryTime = LocalDateTime.now().plusDays(1)
-        )
-
-        coEvery { oidcTokenService.getOidcToken(call) } returns subject1OidcTokenInfo
-        coEvery { wonderwallTokenService.getToken(call) } throws Exception()
-        coEvery { subjectNameService.getSubjectName(subject1) } returns subject1Name
-
-        val subjectInfo = runBlocking { authTokenService.getAuthenticatedUserInfo(call) }
-
-        subjectInfo.authenticated `should be equal to` false
-        subjectInfo.name `should be equal to` null
-        subjectInfo.securityLevel `should be equal to` null
     }
 }
