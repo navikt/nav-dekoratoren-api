0fa9aab132b2c3d8fab71e75a56bd8c81edc1b4d
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-03-05 14:08:16 +0100

    Refactor pga dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    val versions = object {
-        val kotlin = "1.9.22"
-        val shadow = "8.1.1"
-        val versions = "0.51.0"
-    }
+    val kotlinVersion = "1.9.22"
+    val shadowVersion = "8.1.1"
+    val versionsVersion = "0.51.0"
 
-    kotlin("jvm") version(versions.kotlin)
-    kotlin("plugin.allopen") version(versions.kotlin)
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
 
-    id("com.github.johnrengelman.shadow") version(versions.shadow)
-    id("com.github.ben-manes.versions") version(versions.versions) // ./gradlew dependencyUpdates to check for new versions
+    id("com.github.johnrengelman.shadow") version shadowVersion
+    id("com.github.ben-manes.versions") version versionsVersion // ./gradlew dependencyUpdates to check for new versions
     application
 }
 
@@ -25,55 +23,53 @@ repositories {
 }
 
 dependencies {
-    val versions = object {
-        val caffeine = "3.1.8"
-        val dittnavCommon = "2022.09.30-12.41-aa46d2d75788"
-        val tmsKtorTokenSupport = "4.0.0"
-        val jackson = "2.16.1"
-        val junit = "5.10.2"
-        val kluent = "1.73"
-        val kotlinxCoroutines = "1.8.0"
-        val kotlinxHtmlJvm = "0.11.0"
-        val ktor = "2.3.8"
-        val logback = "1.5.0"
-        val logstash = "7.4"
-        val micrometer = "1.12.3"
-        val mockk = "1.13.9"
-        val navSecurity = "4.1.3"
-    }
+    val caffeineVersion = "3.1.8"
+    val dittnavCommonVersion = "2022.09.30-12.41-aa46d2d75788"
+    val tmsKtorTokenSupportVersion = "4.0.0"
+    val jacksonVersion = "2.16.1"
+    val junitVersion = "5.10.2"
+    val kluentVersion = "1.73"
+    val kotlinxCoroutinesVersion = "1.8.0"
+    val kotlinxHtmlJvmVersion = "0.11.0"
+    val ktorVersion = "2.3.8"
+    val logbackVersion = "1.5.0"
+    val logstashVersion = "7.4"
+    val micrometerVersion = "1.12.3"
+    val mockkVersion = "1.13.9"
+    val navSecurityVersion = "4.1.3"
 
-    implementation("com.github.navikt.dittnav-common-lib:dittnav-common-utils:${versions.dittnavCommon}")
-    implementation("com.github.navikt.tms-ktor-token-support:azure-exchange:${versions.tmsKtorTokenSupport}")
-    implementation("com.github.ben-manes.caffeine:caffeine:${versions.caffeine}")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${versions.jackson}")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${versions.jackson}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${versions.kotlinxCoroutines}")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:${versions.kotlinxHtmlJvm}")
-    implementation("io.ktor:ktor-client-apache:${versions.ktor}")
-    implementation("io.ktor:ktor-client-jackson:${versions.ktor}")
-    implementation("io.ktor:ktor-client-json:${versions.ktor}")
-    implementation("io.ktor:ktor-client-logging:${versions.ktor}")
-    implementation("io.ktor:ktor-client-logging-jvm:${versions.ktor}")
-    implementation("io.ktor:ktor-client-serialization-jvm:${versions.ktor}")
-    implementation("io.ktor:ktor-client-content-negotiation:${versions.ktor}")
-    implementation("io.ktor:ktor-server-content-negotiation:${versions.ktor}")
-    implementation("io.ktor:ktor-server-html-builder:${versions.ktor}")
-    implementation("io.ktor:ktor-serialization-jackson:${versions.ktor}")
-    implementation("io.ktor:ktor-server-netty:${versions.ktor}")
-    implementation("io.ktor:ktor-server-default-headers:${versions.ktor}")
-    implementation("io.ktor:ktor-server-cors:${versions.ktor}")
-    implementation("io.ktor:ktor-server-metrics-micrometer:${versions.ktor}")
-    implementation("io.micrometer:micrometer-registry-prometheus:${versions.micrometer}")
-    implementation("ch.qos.logback:logback-classic:${versions.logback}")
-    implementation("net.logstash.logback:logstash-logback-encoder:${versions.logstash}")
-    implementation("no.nav.security:token-validation-ktor-v2:${versions.navSecurity}")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${versions.junit}")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:${versions.junit}")
-    testImplementation("org.amshove.kluent:kluent:${versions.kluent}")
-    testImplementation("io.ktor:ktor-client-mock:${versions.ktor}")
-    testImplementation("io.ktor:ktor-client-mock-jvm:${versions.ktor}")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:${versions.ktor}")
-    testImplementation("io.mockk:mockk:${versions.mockk}")
+    implementation("com.github.navikt.dittnav-common-lib:dittnav-common-utils:$dittnavCommonVersion")
+    implementation("com.github.navikt.tms-ktor-token-support:azure-exchange:$tmsKtorTokenSupportVersion")
+    implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlJvmVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-html-builder:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    implementation("no.nav.security:token-validation-ktor-v2:$navSecurityVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktorVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
 }
 
 application {
