63ba1e0bd10855a84dfd726e1984c6f40c13e585
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-06-03 10:32:21 +0200

    Dropper retries, da dette ser ut til å føre til enorm minnebruk og treg respons når PDL er nede

--- src/main/kotlin/no/nav/dekoratoren/api/config/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/dekoratoren/api/config/HttpClientBuilder.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
-import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.jackson.jackson
@@ -19,14 +18,8 @@ object HttpClientBuilder {
                     configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 }
             }
-            install(HttpRequestRetry) {
-                retryOnServerErrors(maxRetries = 2)
-                retryOnException(maxRetries = 2, retryOnTimeout = true)
-                constantDelay(100)
-            }
             install(HttpTimeout) {
-                requestTimeoutMillis = 2000
-                connectTimeoutMillis = 1000
+                requestTimeoutMillis = 1000
             }
         }
     }
