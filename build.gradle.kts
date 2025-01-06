678f8460661c24752b244656b23b1b77b7f167a7
taniaholst <tania.holst@nav.no>
2025-01-06 07:59:13 +0100

    :arrow_up: Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     val micrometerVersion = "1.14.2"
     val mockkVersion = "1.13.14"
     val navSecurityVersion = "5.0.13"
-    val tmsKtorTokenSupportVersion = "5.0.0"
+    val tmsKtorTokenSupportVersion = "5.0.1"
 
     implementation("com.github.navikt.dittnav-common-lib:dittnav-common-utils:$dittnavCommonVersion")
     implementation("no.nav.tms.token.support:azure-exchange:$tmsKtorTokenSupportVersion")
