8a1f6e3a2a2330c1d5ef7839b1da3d75b2e99c1d
taniaholst <tania.holst@nav.no>
2025-02-10 08:34:50 +0100

    bumps og endre deprecated kode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    val kotlinVersion = "2.1.0"
+    val kotlinVersion = "2.1.10"
     val shadowVersion = "8.1.1"
     val versionsVersion = "0.52.0"
 
