a54a922cfb648d835c7fbcc442ec6049dddfe0fc
Tania Holst <holst.tania@gmail.com>
2025-12-18 10:00:36 +0100

    minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     val ktorVersion = "3.3.3"
     val logbackVersion = "1.5.22"
     val logstashVersion = "9.0"
-    val micrometerVersion = "1.15.5"
+    val micrometerVersion = "1.16.1"
     val mockkVersion = "1.14.7"
 
     val navSecurityVersion = "5.0.39"
